8d875e6f68e90223f85706ef310b7093f1abd01c
Micah R Ledbetter <me@micahrl.com>
2026-08-09 20:30:14 -0500

    Use github-dark theme

--- docs/antedom.js
+++ docs/antedom.js
@@ -2,7 +2,7 @@ antedom.apiVersion(1);
 
 antedom.on("page:document", (page) => {
   page.document.highlight({
-    style: "github",
+    style: "github-dark",
     unknownLanguage: "ignore",
   });
 });
